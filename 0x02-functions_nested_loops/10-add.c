1b600b21cdca37268f85f21f089405564bbe36fb
root <root@i386>
2023-02-21 14:34:57 +0200

    Diving into the C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * add - adds 2 integers
+ * n1 - integer 1
+ * n2 - integer 2
+ * description: gives two integers, returns sum
+ * return: sum
+ * */
+
+int add(int 1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * _islower - entry point
+ * description: checks if a character is lowercase
+ * c: the integer value it receives
+ * return: 1 if true, 0 if false.
+ */
+int _islower(int C)
+{
+int i = 'a';
+
+for (i = 'a'; i <= 'z'; i++)
+{
+/* assign value to int c */
+if (c == i)
+{
+return(1);
+}
+}
+return(0);
+}
