e0b866a23809172931a89869753712e26518b5a1
Linf0rd <linfordsnagger@gmx.com>
2023-03-05 09:19:09 +0200

    1-main.c amendment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-
-/*
- * main - causes an infinite loop
+/**
+ * main - avoids an infinite loop
  * Return: 0
- * */
-
+ */
 int main(void)
 {
 	int i;
@@ -12,14 +9,15 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/*
-	*while (i < 10)
-	*{
-	*	putchar(i);
-	*
-	* }
-	*/
-printf("Infinite loop avoided \\o/\n");
 
-return(0);
-} 
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
