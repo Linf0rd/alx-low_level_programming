16fb5a7a9b52c943d13a371c04b7693839fd1cce
Linf0rd <linfordsnagger@gmx.com>
2023-03-05 09:22:58 +0200

    3-print_remaining_days.c amendment

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,11 +9,11 @@
  * @day: day of month
  * @year: year
  * Return: void
- * */
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0 ) || (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 		if (month > 2 && day >= 60)
 		{
@@ -21,7 +21,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		printf("Day of the year: %d\n", day)
-		printf("Remaining days: %d\n", 366 -day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -32,7 +32,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 -day);
+		printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
