ded1e6cc27d83574c085a4f7226709ce4445400a
Linf0rd <linfordsnagger@gmx.com>
2023-03-11 15:37:40 +0200

    Amend

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-    /* Print the name of the program */
-    printf("%s\n", argv[0]);
 
-    /* Exit the program */
-    return (0);
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
