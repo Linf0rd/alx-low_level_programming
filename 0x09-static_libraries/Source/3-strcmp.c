caf7fcb694ac4af4d18989787cb4dfa7a98c3ea0
Linf0rd <linfordsnagger@gmx.com>
2023-03-11 13:35:35 +0200

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/Source/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: an integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if (s1[i] == '\0')
+			return (0);
+	}
+
+	return (s1[i] - s2[i]);
+}
