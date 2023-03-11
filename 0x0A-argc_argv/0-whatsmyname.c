271897da6bc6c53af0eba36a76e260761b31b8a3
Linf0rd <linfordsnagger@gmx.com>
2023-03-11 15:41:40 +0200

    C Breeze

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: Always 0 (Success)
+ * main - Prints the program name
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-
+	(void)argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
