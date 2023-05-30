0537af78994b69c4e56867167a78788d72cf2202
Linf0rd <linfordsnagger@gmx.com>
2023-05-30 16:53:18 +0200

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,17 +15,22 @@ void times_table(void)
 			product = x * y;
 
 			if (y == 0)
+			{
 				_putchar('0');
+			}
 			else if (product < 10)
-				_putchar(' ');
-			else
-				_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
-
-			if (y != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 		_putchar('\n');
