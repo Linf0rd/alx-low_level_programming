57594ebfce3c1a7a5df8fe2dcea511e050dbfc51
Linf0rd <linfordsnagger@gmx.com>
2023-06-17 01:23:02 +0200

    Implement malloc_checked function
    Implements the malloc_checked function that allocates memory using malloc. The function takes in b as the number of bytes to allocate. It uses malloc to allocate memory of size b and assigns the returned pointer to ptr. Then, it checks if ptr is NULL, indicating that the memory allocation has failed. In such case, the function terminates the process with status 98, ensuring a graceful exit. If ptr is not NULL, it returns the pointer to the allocated memory. This function provides a reliable way to allocate memory and handle allocation failures.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ *		if malloc fails, the function terminates the process,
+ *		with status.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
