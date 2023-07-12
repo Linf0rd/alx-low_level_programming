965fa12aae273cae2189afd4d37b1dabe6e95f16
Linf0rd <linfordsnagger@gmx.com>
2023-07-12 08:46:10 +0200

    Add function to safely free a linked list
    Implement the free_listint_safe function, which frees a listint_t linked list safely. The function takes a pointer to the head of the list. It initializes a current pointer to iterate through the list. It increments the count variable and checks if the current pointer is less than or equal to the next pointer, indicating a loop in the list. If a loop is detected, it sets the head pointer to NULL, frees the current node, and breaks the loop to prevent infinite freeing. Otherwise, it assigns the current node to a temporary pointer, updates the current pointer to the next node, and frees the temporary node. The function returns the size of the list that was freed. This function provides a safe way to free a linked list without getting stuck in an infinite loop.

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The size of the list that was freed.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *temp;
+	size_t count = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+	while (current != NULL)
+	{
+		count++;
+		if (current <= current->next)
+		{
+			*h = NULL;
+			free(current);
+			break;
+		}
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+
+	return (count);
+}
