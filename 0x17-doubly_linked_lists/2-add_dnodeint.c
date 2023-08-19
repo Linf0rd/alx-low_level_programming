0d6172e5cf320ad08dc1fed1e3b8bfc75f97bb9b
Linf0rd <linfordsnagger@gmx.com>
2023-08-19 16:52:27 +0200

    Add a node at the beginning of a doubly linked list
    Implemented 'add_dnodeint' function to add a new node at the beginning of a doubly linked list. The function creates a new node, assigns the given value to it, adjusts the next and prev pointers, and updates the head pointer.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n n;
+	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 
